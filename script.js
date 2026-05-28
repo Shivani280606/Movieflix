aa870837193e8b309a07f68752136c00c4e92111
Shivani280606 <shivanivm2006@gmail.com>
2026-05-28 10:54:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("MovieFlix Loaded 🎬");
+```javascript
+console.log("🎬 MovieFlix Loaded Successfully");
+
+/* Heart Toggle */
+
+const hearts = document.querySelectorAll(".fa-heart");
+
+hearts.forEach((heart) => {
+
+  heart.addEventListener("click", () => {
+
+    heart.classList.toggle("fa-solid");
+
+    heart.style.color = "#ff2e63";
+
+  });
+
+});
+```
